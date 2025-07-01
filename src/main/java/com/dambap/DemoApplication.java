2a5e4c5a02ead26f84c88d780dc52be5e9539531
OhHaneol <otyvs1109@hs.ac.kr>
2025-07-01 14:48:11 +0900

    chore: change directory location

--- src/main/java/com/dambap/DemoApplication.java
+++ src/main/java/com/dambap/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.wandoo.demo;
+package com.dambap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dambap/DemoApplicationTests.java
+++ src/test/java/com/dambap/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wandoo.demo;
+package com.dambap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
