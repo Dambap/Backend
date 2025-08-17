fa4d58729e425390351b5d4837b95637b85a630b
OhHaneol <otyvs1109@hs.ac.kr>
2025-08-17 13:45:39 +0900

    feat: 사용자 태그 기능 도메인 및 dto, 저장소 개발

---
+++ src/main/java/com/double_o/dambap/post/domain/TaggedUser.java
@@ -0,0 +1,33 @@
+package com.double_o.dambap.post.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tagged_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class TaggedUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "tagged_user_id")
+    private Long taggedUserId;
+
+    private int sequence;
+}
---
+++ src/main/java/com/double_o/dambap/post/infrastructure/TaggedUserRepository.java
@@ -0,0 +1,18 @@
+package com.double_o.dambap.post.infrastructure;
+
+import com.double_o.dambap.post.domain.TaggedUser;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface TaggedUserRepository extends JpaRepository<TaggedUser, Long> {
+
+    void deleteAllByPostId(Long postId);
+
+    @Query("SELECT tu FROM TaggedUser tu WHERE tu.postId=:postId ORDER BY tu.sequence ASC")
+    List<TaggedUser> findAllByPostIdOrBOrderBySequenceAsc(Long postId);
+}
