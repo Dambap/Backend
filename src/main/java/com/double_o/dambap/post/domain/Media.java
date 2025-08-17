938216310cf11a0d2cb58b72cb7511d4f49c4dbd
OhHaneol <otyvs1109@hs.ac.kr>
2025-08-17 13:41:24 +0900

    feat: 미디어 관련 도메인 및 저장소 개발

---
+++ src/main/java/com/double_o/dambap/post/domain/Media.java
@@ -0,0 +1,37 @@
+package com.double_o.dambap.post.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "media")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Media {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "media_url")
+    private String mediaUrl;
+
+    @Enumerated
+    private Type type;
+
+    private int sequence;   // 사용자가 이미지를 보낸 순서
+}
