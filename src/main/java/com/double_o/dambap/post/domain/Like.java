e7346e66ad20a56f42ee1183bc1eda185c9e91e6
OhHaneol <otyvs1109@hs.ac.kr>
2025-08-17 13:43:12 +0900

    feat: 좋아요 기능 도메인 및 dto, 저장소 개발

---
+++ src/main/java/com/double_o/dambap/post/domain/Like.java
@@ -0,0 +1,35 @@
+package com.double_o.dambap.post.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "like")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "liked_at")
+    private LocalDate likedAt;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "liker_id")
+    private Long likerId;
+}
