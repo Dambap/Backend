e24f18c6448eefabcd299798dac5d38a8b69d02f
OhHaneol <otyvs1109@hs.ac.kr>
2025-09-01 01:21:42 +0900

    feat: post_like 테이블 인덱스 추가로 조회 성능 개선

--- src/main/java/com/double_o/dambap/post/domain/PostLike.java
+++ src/main/java/com/double_o/dambap/post/domain/PostLike.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
@@ -13,7 +14,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "post_like")
+//@Table(name = "post_like")
+// 조회 성능을 위한 JPA 이용 인덱스 추가
+@Table(
+        name = "post_like",
+        indexes = {
+                @Index(name = "idx_postlike_postid", columnList = "post_id"),
+                @Index(name = "idx_postlike_likerid", columnList = "liker_id"),
+                @Index(name = "uq_postlike_postid_likerid", columnList = "post_id, liker_id", unique = true)
+        }
+)
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
